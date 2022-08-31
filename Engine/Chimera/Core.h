0e508f5c1005427e803a06c335507f1d6baa503a
LtSnail <frogressivemetal@gmail.com>
2022-08-31 20:40:06 +0400

    Add asserts

--- Engine/Chimera/Core.h
+++ Engine/Chimera/Core.h
@@ -10,6 +10,19 @@
     #error Unsupported platform!
 #endif
 
+#if defined(CHIMERA_ENABLE_ASSERTS)
+    #if defined(CHIMERA_PLATFORM_WINDOWS)
+        #define CH_ASSERT(x, ...) { if(!(x)) { CH_ERROR("Assertion Failed {0}", __VA_ARGS__); __debugbreak(); } }
+        #define CH_CORE_ASSERT(x, ...) { if(!(x)) { CH_CORE_ERROR("Assertion Failed {0}", __VA_ARGS__); __debugbreak(); } }
+    #elif defined(CHIMERA_PLATFORM_MACOS)
+        #define CH_ASSERT(x, ...) { if(!(x)) { CH_ERROR("Assertion Failed {0}", __VA_ARGS__); __builtin_debugtrap(); } }
+        #define CH_CORE_ASSERT(x, ...) { if(!(x)) { CH_CORE_ERROR("Assertion Failed {0}", __VA_ARGS__); __builtin_debugtrap(); } }
+    #endif
+#else
+    #define CH_ASSERT(x, ...)
+    #define CH_CORE_ASSERT(x, ...) 
+#endif
+
 #if defined(CHIMERA_BUILD_LIB)
     #define CHIMERA_API EXPORT
 #else
